f69cb009cb5c133783bdfa0bf24bb91e978e1652
dhruvpapdeja06 <dhruv2024papdeja@gmail.com>
2026-01-23 15:30:24 +0530

    Coder_Yt , Internal of UseState Hook

--- Coder_Youtube/Day-4/useState/src/App.jsx
+++ Coder_Youtube/Day-4/useState/src/App.jsx
@@ -1,26 +1,74 @@
-import React from 'react'
+// What is hook, why we need it and what exactly the problem they solve.
+
+// StrictMode use at development time
+
+// Here value of counter is increased on console but not display on the UI, 
+
+// Why we don't perform direct DOM Mainputlation by own.
+
+// We have not perform the DOM Manipulation. In React we have useState for better Optimization.
+
+// Hooks is nothing basically a powerful function.
+
+// Check the internal implementation of useState
+
+import React ,{ useState } from 'react'
+// import React from 'react'
 
 const App = ()=>{
   
-  let count = 0;
+  // For Not create varible like this
+  // let count = 0;
+
+
+  // useState Hook --> first arg , is hold the value of state , 2nd arg is function
+  const [count,setCount] = useState(0); // return an array 
+
+
+  // This will create source of bug(Cards component is same , change by props),manual update, Need this counter and increment 2 more.
+  // Component --> Re usable use many times.
+  // function incrementNumber(){
+  //   count++;
+  //   const h2 = document.querySelector("h2");
+  //   h2.innerText = `Count is : ${count}`;
+  //   console.log(`Increment is clicked, Count is : ${count}`);
+  // }
+
+  // function decrementNumber(){
+  //   count--;
+  //   const h2 = document.querySelector("h2");
+  //   h2.innerText = `Count is : ${count}`;
+  //   console.log(`Decrement is clicked, Count is : ${count}`);
+  // }
+  
+  // In component Part when we re-use it changes the first one not second so it is bug or something else.
+  // This is bug of JS (DOM Manipulation we do)
 
   function incrementNumber(){
-    count++;
-    console.log(`Increment is clicked, Count is : ${count}`);
+    // count++;
+    // count = count + 1;
+    // setCount update the value of count and Re-render the App function
+    setCount(count + 1);
   }
 
   function decrementNumber(){
-    count--;
-    console.log(`Decrement is clicked, Count is : ${count}`);
+    // count --;
+    setCount(count - 1);
   }
-  
 
   
   return (
     <div className="parent">
       <h2>Count is : {count}</h2>
       {/* Pass the function reference not call it incrementNumber() otherwise it call it */}
-      <button id="btnInc" onClick={incrementNumber}>Increment</button>
+      {/* If i want to use the count variable multiple place for that i have to select the element and
+      perform DOM manipulation on it. In this way there is high possibility of mistake(Here manual work increase) */}
+      
+      {/* when the value of count is changed , there is any way i give the signal to react and perform
+      all the manipulation (Behind the scene React DOM do it but here i don't take the headache.) */}
+      
+      
+      <button id="btnInc" onClick={incrementNumber}>Increment </button>
       <button id="btnDesc" onClick={decrementNumber}>Decrement</button>
     </div>
   )
--- Coder_Youtube/Day-4/useState/src/main.jsx
+++ Coder_Youtube/Day-4/useState/src/main.jsx
@@ -6,5 +6,28 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* React effeciently handle the manipulation , how it handle it */}
+
+    {/*       Root   
+        App         App
+    counter code(count = 0)  counter code (count = 0)
+    
+    // increse function call increase the value of count , we use queryselector select the first h2,button in the code we have 2 of it
+
+    count --> 1,2 when press left child button 
+
+    Update the value of the first
+    
+    when press right child button start count = 1 (count is local variable of right child)
+
+    React manage the state and based on that they change it (internally not change the otherone
+    
+    state kis particular function sa attach hai
+
+    Ex- open video of coder but play music , then user experience is not good.
+    )
+    */}
+
+    {/* <App /> */}
   </StrictMode>,
 )
